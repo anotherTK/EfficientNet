804ce5e0048cf2d27c658f6d825edb1f84caba97
tkianai <tkianai@163.com>
2019-06-13 19:50:52 +0800

    minor

--- utils.py
+++ utils.py
@@ -77,7 +77,7 @@ class MetricLogger(object):
 
     def update(self, **kwargs):
         for k, v in kwargs.items():
-            if isinstance(v. torch.Tensor):
+            if isinstance(v, torch.Tensor):
                 v = v.item()
             assert isinstance(v, (float, int))
             self.meters[k].update(v)
